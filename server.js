1e1448f99f5759f33d5e6b4a7979f3aabfb5350d
ishaan__33k <ishaankhare31@gmail.com>
2021-04-07 17:03:34 +0530

    initial

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const ejs = require('ejs');
+const expressLayout = require('express-ejs-layouts');
+const app = express();
+const path = require('path');
+app.get("/",(req, res) =>{
+    res.render('home');
+});
+
+app.use(expressLayout);
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs');
+const PORT = process.envPORT || 3001;
+
+app.listen(PORT, ()=>{
+    console.log(`server started on ${ PORT }`);
+});
