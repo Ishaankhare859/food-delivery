427e1601acc9371b66509ff31e050e036e167d21
ishaan__33k <ishaankhare31@gmail.com>
2021-04-09 21:23:37 +0530

    cart

--- server.js
+++ server.js
@@ -3,16 +3,28 @@ const ejs = require('ejs');
 const expressLayout = require('express-ejs-layouts');
 const app = express();
 const path = require('path');
-app.get("/",(req, res) =>{
-    res.render('home');
-});
+
+
 //assests
 app.use(express.static('public'));
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs');
-const PORT = process.envPORT || 3001;
+const PORT = process.envPORT || 3002;
 
+app.get("/cart",(req, res) =>{
+    res.render('customers/cart');
+});
+
+app.get("/signup",(req, res) =>{
+    res.render('auth/signup');
+});
+app.get("/signin",(req, res) =>{
+    res.render('auth/signin');
+});
+app.get("/",(req, res) =>{
+    res.render('home');
+});
 app.listen(PORT, ()=>{
     console.log(`server started on ${ PORT }`);
 });
